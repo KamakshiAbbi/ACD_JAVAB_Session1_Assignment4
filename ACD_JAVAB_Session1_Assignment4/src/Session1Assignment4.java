162f8e744fa8d343bae04bcf231d0e5de5f053ef
KamakshiAbbi <kamakshigit12@gmail.com>
2016-04-24 02:48:23 +0530

    ACD_Session1Assignment4
    Solving Expressions

---
+++ ACD_JAVAB_Session1_Assignment4/src/Session1Assignment4.java
@@ -0,0 +1,16 @@
+//Session1_Assignment4 : 23.4.016
+//Author : Kamakshi Abbi
+public class Session1Assignment4 {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int exp = x + y * 2;
+		System.out.println("x + y * 2 = " + exp);
+		exp = x - y + 2;
+		System.out.println("x - y + 2 = "+ exp);
+		exp = (x+y) * 2;
+		System.out.println("(x + y)* 2 = "+ exp);
+		exp = y % x;
+		System.out.println("y % x = " + exp);
+	}
+}
